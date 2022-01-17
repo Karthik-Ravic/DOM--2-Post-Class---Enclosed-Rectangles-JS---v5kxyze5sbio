bca343d096278a9b549b47f08fbfac884566c685
Karthik Ravichandran <karthikravichandran1991@gmail.com>
2022-01-17 13:49:34 +0530

    updateStructure function has been completed

--- src/components/index.js
+++ src/components/index.js
@@ -27,43 +27,60 @@ function updateStructure(rec1,rec2){
 	rec2Top = Number(rec2Top.substr(0, rec2Top.length-2));
 	let rec2Height = rec2.height;
 	rec2Height = Number(rec2Height.substr(0, rec2Height.length-2));
-	if(rec1Top <= rec2Top) {
-		if(rec1Left<= rec2Left) {
-			if(rec1Left+ rec1Width >= rec2Left+rec2Width) {
-				if(rec1Top + rec1Height >= rec2Top + rec2Height) {
-					rec2Top = rec2Top - rec1Top;
-					rec2Left = rec2Left - rec1Left;
-					let ans1 = {...rec1}; 
-					ans1.children = {
-						top: rec2Left+"px",
-						left: rec2Top + "px",
-						width: rec2Width+ "px",
-						height: rec2Height + "px"
-					}
-					return ans1;
-				}
-			}
-		}
-	} else if(rec1Top >= rec2Top) {
-		if(rec1Left>= rec2Left) {
-			if(rec1Left+ rec1Width <= rec2Left+rec2Width) {
-				if(rec1Top + rec1Height <= rec2Top + rec2Height) {
-					rec1Top = rec1Top - rec2Top;
-					rec1Left = rec1Left - rec2Left;
-					let ans2 = {...rec2}; 
-					ans2.children = {
-						top: rec1Left+"px",
-						left: rec1Top + "px",
-						width: rec1Width+ "px",
-						height: rec1Height + "px"
-					}
-					return ans2;
-				}
-			}
-		}
-	} else {
-		return {...rec1};
-	}
+  function checkA() {
+  	if(rec1Top <= rec2Top) {
+  
+      if(rec1Left<= rec2Left) {
+
+        if(rec1Left+ rec1Width >= rec2Left+rec2Width) {
+
+          if(rec1Top + rec1Height >= rec2Top + rec2Height) {
+          console.log('here');
+            rec2Top = rec2Top - rec1Top;
+            rec2Left = rec2Left - rec1Left;
+            let ans1 = {...rec1}; 
+            ans1.children = {
+              top: rec2Left+"px",
+              left: rec2Top + "px",
+              width: rec2Width+ "px",
+              height: rec2Height + "px"
+            }
+            return ans1;
+          }
+          return checkB()
+        }
+        return checkB()
+      }
+      return checkB()
+    }
+    return checkB()
+  }
+  function checkB() {
+  	if(rec1Top >= rec2Top) {
+      if(rec1Left>= rec2Left) {
+        if(rec1Left+ rec1Width <= rec2Left+rec2Width) {
+          if(rec1Top + rec1Height <= rec2Top + rec2Height) {
+            rec1Top = rec1Top - rec2Top;
+            rec1Left = rec1Left - rec2Left;
+            let ans2 = {...rec2}; 
+
+            ans2.children = {
+              top: rec1Left+"px",
+              left: rec1Top + "px",
+              width: rec1Width+ "px",
+              height: rec1Height + "px"
+            }
+            return ans2;
+          }
+					return {...rec1};
+        }
+        return {...rec1};
+      }
+      return {...rec1};
+    }
+    return {...rec1};
+  }
+ return checkA()
 }
 
 module.exports = updateStructure;
